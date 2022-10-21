a9b645aa6cdce4767643769c811f7b212bce3bca
KruseCtrl <61332419+KruseCtrl@users.noreply.github.com>
2022-10-21 11:19:47 +0100

    Update CIUK-generic-test.sh

--- CIUK-generic-test.sh
+++ CIUK-generic-test.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-sudo dnf install -y cowsay
-sudo dnf install -y fortune-mod
-sudo rm /usr/share/cowsay/cows/telebears.cow
 
-fortune | cowsay -f `ls /usr/share/cowsay/cows/ | shuf -n 1`
-fortune | cowsay -f `ls /usr/share/cowsay/cows/ | shuf -n 1` 1>"/home/flight/test.out" 2>>/dev/null
-fortune | cowsay -f `ls /usr/share/cowsay/cows/ | shuf -n 1` 1>"/home/centos/test.out" 2>>/dev/null
-fortune | cowsay -f `ls /usr/share/cowsay/cows/ | shuf -n 1` 1>"/home/root/test.out" 2>>/dev/null
+echo "just a generic test script here" > /home/flight/test.out
+echo "just a generic test script here" > /home/centos/test.out
+echo "just a generic test script here" > /home/root/test.out
