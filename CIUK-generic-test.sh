75f0ac2ce89620e0d3c957b7b3057a9a4ffe1162
KruseCtrl <61332419+KruseCtrl@users.noreply.github.com>
2022-10-21 15:11:57 +0100

    show starting

--- CIUK-generic-test.sh
+++ CIUK-generic-test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sudo dnf install -y cowsay
-sudo dnf install -y fortune-mod
+echo "Starting script, cows incoming" > /tmp/generic-test.log
+sudo dnf install -y cowsay fortune-mod
 
 # remove telebears
 sudo rm $(find /usr/share/cowsay/ -name "telebears.cow")
 
-fortune | cowsay -f `ls /usr/share/cowsay/ | shuf -n 1` 1>"/var/log/generic-test.log"
+fortune | cowsay -f `ls /usr/share/cowsay/ | shuf -n 1` 1>"/tmp/generic-test.log"
+
